6f1fbde87ccfaade47351b0d676f9eb9048eac22
Mehdi Maleki <mehdi.maleki@ri.se>
2022-06-02 16:08:40 +0200

    ComFASE

--- src/comfase/injectorVeins/injectorV.cc
+++ src/comfase/injectorVeins/injectorV.cc
@@ -1,45 +1,19 @@
-//
-// Copyright (C) 2004 Telecommunication Networks Group (TKN) at Technische Universitaet Berlin, Germany.
-//
-// Documentation for these modules is at http://veins.car2x.org/
-//
-// SPDX-License-Identifier: GPL-2.0-or-later
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-//
 
-// author:      Marc Loebbers
-// part of:     framework implementation developed by tkn
-// description: - Base class for physical layers
-//              - if you create your own physical layer, please subclass
-//                from this class and use the sendToChannel() function!!
 
+#include "../injectorVeins/injectorV.h"
 
 #include "omnetpp.h"
-#include "veins/attackInjection/Injector.h"
 using std::endl;
 using namespace omnetpp;
-
+#include <algorithm>
 // Here we define the module to be able to call from main ned file (for instance from "Highway.ned" file in platooning scenario)
-Define_Module(Injector);
+Define_Module(injectorV);
 
 
 /*########################################################################
 # Initialization of the defined parameters value                         #
 ##########################################################################*/
-void Injector::initialize()
+void injectorV::initialize()
 {
     //    //Mehdi
     myPDValue        = par("myPDValue").doubleValue();
@@ -52,17 +26,39 @@ void Injector::initialize()
     attackNode       = par("attackNode").intValue();
     DelayAttack      = par("DelayAttack").boolValue();
     DoSAttack        = par("DoSAttack").boolValue();
-
+   
+    const char* strNodes = par("myNodes").stringValue();
+    //const char str = myNodes;
+    std::vector<double> vehicleList = cStringTokenizer(strNodes).asDoubleVector();
+    std::cout<<"double list=========================================================================> "<<vehicleList.at(1)<<std::endl;
+    //vList = vehicleList;
+    vList.assign (vehicleList.begin(),vehicleList.end());
+    std::cout<<"double list=========================================================================> "<<vList[1]<<std::endl;
+    t_m = attackStartTime + 1.0;
+}
+/*########################################################################
+# Attack Injection constructor for checking vehicle ID          #
+##########################################################################*/
+bool injectorV::checkVehiclesList(int vehicleID)
+{
+    bool result = false;
+    if(std::find(vList.begin(), vList.end(), vehicleID) != vList.end() )
+    {
+        result = true;
+    }
+    return result;
 }
 /*########################################################################
 # Attack Injection constructor for "PropagationDelay" parameter          #
 ##########################################################################*/
-float Injector::PropagationDelayAttack(int senderID, int receiverID, float correctValue)
+float injectorV::PropagationDelayAttack(int senderID, int receiverID, float correctValue)
 {
+    bool senderPart = injectorV::checkVehiclesList(senderID);
+    bool receiverPart = injectorV::checkVehiclesList(receiverID);
     // Attack on SENDER part
     if (simTime()>=attackStartTime && simTime()<=attackEndTime && attackOnSender && !attackOnReceiver){
         std::cout<<"AttackActive = condition 1"<<std::endl;
-        if (senderID==attackNode){
+        if (senderPart){
             return myPDValue;
         }
         else{
@@ -71,7 +67,7 @@ float Injector::PropagationDelayAttack(int senderID, int receiverID, float corre
     // Attack on RECEIVER part
     else if (simTime()>=attackStartTime && simTime()<=attackEndTime && !attackOnSender && attackOnReceiver){
         std::cout<<"AttackActive = condition 2"<<std::endl;
-        if (receiverID==attackNode){
+        if (receiverPart){
             return myPDValue;
         }
         else{
@@ -80,7 +76,7 @@ float Injector::PropagationDelayAttack(int senderID, int receiverID, float corre
     // Attack on SENDER and RECEIVER parts (both sides; then we use or (||) to apply this)
     else if (simTime()>=attackStartTime && simTime()<=attackEndTime && attackOnSender && attackOnReceiver){
         std::cout<<"AttackActive = condition 3"<<std::endl;
-        if (senderID==attackNode || receiverID==attackNode){
+        if (senderPart || receiverPart){
             return myPDValue;
         }
         else{
@@ -97,7 +93,7 @@ float Injector::PropagationDelayAttack(int senderID, int receiverID, float corre
 # Attack Injection constructor for "Denial of Service Attack" parameter  #
 ##########################################################################*/
 
-float Injector::DenialOfServiceAttack(int senderID, int receiverID, float correctValue)
+float injectorV::DenialOfServiceAttack(int senderID, int receiverID, float correctValue)
 {
     // Attack on SENDER part
     if (simTime()>=attackStartTime && attackOnSender && !attackOnReceiver){
@@ -119,8 +115,9 @@ float Injector::DenialOfServiceAttack(int senderID, int receiverID, float correc
         }}
     // Attack on SENDER and RECEIVER parts (both sides; then we use or (||) to apply this)
     else if (simTime()>=attackStartTime && attackOnSender && attackOnReceiver){
-        std::cout<<"AttackActive = condition 3"<<std::endl;
+        //std::cout<<"AttackActive = condition 3"<<std::endl;
         if (senderID==attackNode || receiverID==attackNode){
+            std::cout<<"AttackActive = condition 3"<<std::endl;
             return myPDforDoS;
         }
         else{
@@ -134,21 +131,4 @@ float Injector::DenialOfServiceAttack(int senderID, int receiverID, float correc
 }
 
 
-/*########################################################################
-# Attack Injection Engine  #
-##########################################################################*/
 
-float Injector::AttackInjectionEngine(int senderID, int receiverID, float correctValue)
-{
-    // Delay Attack
-    if (DelayAttack){
-        return PropagationDelayAttack(senderID, receiverID, correctValue);
-    }
-    // Attack on RECEIVER part
-    else if (DoSAttack){
-        return DenialOfServiceAttack(senderID, receiverID, correctValue);
-        }
-    else {
-        return correctValue;
-    }
-}
--- src/comfase/injectorVeins/injectorV.h
+++ src/comfase/injectorVeins/injectorV.h
@@ -1,37 +1,9 @@
-//
-// Copyright (C) 2004 Telecommunication Networks Group (TKN) at Technische Universitaet Berlin, Germany.
-//
-// Documentation for these modules is at http://veins.car2x.org/
-//
-// SPDX-License-Identifier: GPL-2.0-or-later
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-//
-
-// author:      Marc Loebbers
-// part of:     framework implementation developed by tkn
-// description: - Base class for physical layers
-//              - if you create your own physical layer, please subclass
-//                from this class and use the sendToChannel() function!!
-
 #pragma once
-
+#include <string>
 #include "omnetpp.h"
 using namespace omnetpp;
 
-class Injector : public cSimpleModule
+class injectorV : public cSimpleModule
 {
 protected:
     virtual void initialize();
@@ -60,13 +32,16 @@ public:
     bool DelayAttack;
     bool DoSAttack;
 
+    std::string myNodes;
+    std::vector<int> vList;
+    //std::list<int> vList;
+    double t_m;
 
-    //void PropagationDelayAttack();
     float PropagationDelayAttack(int senderID, int receiverID, float correctValue);
     float DenialOfServiceAttack(int senderID, int receiverID, float correctValue);
-    float AttackInjectionEngine(int senderID, int receiverID, float correctValue);
     void BitFlipAttack();
     void JammingAttack();
+    bool checkVehiclesList(int vehicleID);
     //void initialize(int stage) override;
 //protected:
 //    /** @brief Initialization of the module and some variables.*/
